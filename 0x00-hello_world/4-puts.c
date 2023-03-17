a1e1461ee07d0176da65da3f78fce0c14e051274
anassYinssi <anass.yinssi@outlook.fr>
2023-03-17 09:04:48 -0700

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+
+/**
+ * main - A program that prints a line with puts f
+ * unction
+ *
+ * Return: 0 (Success)
+ */
+
 int main(void)
 {
-	puts(" \" Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }

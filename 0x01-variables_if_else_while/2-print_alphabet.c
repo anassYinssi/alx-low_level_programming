e89ae5082a9e090ff239f690e831caa950363e7a
anassYinssi <anass.yinssi@outlook.fr>
2023-03-17 17:22:45 -0700

    third question

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ *  main - print alphabet
+ *
+ *  Return: always 0 (Success)
+ */
+
+int main(void)
+{
+	char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alphabet[i]);
+	}
+	putchar('\n');
+	return (0);
+}

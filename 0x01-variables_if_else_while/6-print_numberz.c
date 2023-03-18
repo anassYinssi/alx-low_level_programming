2d262982cc3d2dee5b8e001826cbcf6e3ca879e5
anassYinssi <anass.yinssi@outlook.fr>
2023-03-17 18:21:30 -0700

    6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ *  main - print alphabet
+ *
+ *  Return: always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 9; i++)
+	{
+		putchar(n + 48);
+	}
+	putchar('\n');
+	return (0);
+}

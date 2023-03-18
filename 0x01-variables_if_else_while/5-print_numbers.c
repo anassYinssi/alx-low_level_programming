17c9b81dedf0fde87691a4e26474860b53327374
anassYinssi <anass.yinssi@outlook.fr>
2023-03-17 18:14:13 -0700

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ *  main - print alphabet
+ *
+ *  Return: always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10 ; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}

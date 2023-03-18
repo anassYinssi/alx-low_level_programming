647b9c56c0c76cf01ecd998b87e5e6d955cb404a
anassYinssi <anass.yinssi@outlook.fr>
2023-03-17 18:42:27 -0700

    8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ *  main - print alphabet
+ *
+ *  Return: always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+	int m;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	for (m = 97; m <= 102; n++)
+	{
+		putchar(m);
+	}
+
+	return (0);
+}

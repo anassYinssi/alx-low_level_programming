2cc71a299b2e69c1630a8d3c0e447a30f362f8c3
anassYinssi <anass.yinssi@outlook.fr>
2023-03-21 16:48:23 -0700

    5 question

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+* print_sign - Program entry
+* @n: number to test
+* Return: Always 0 (Success)
+*/
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		return (0);
+	}
+}

942edfcf03c5c4d84f57ba8c2def2e9ba90842e0
anassYinssi <anass.yinssi@outlook.fr>
2023-03-21 06:50:35 -0700

    3 question

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print_alphabet_x10 - Program entry
+*
+* Return: Always 0 (Success)
+*/
+
+void print_alphabet_x10(void)
+{
+	int i =0;
+	do
+	{
+		print_alphabet();
+		i++;
+	}
+	while (i < 10);
+}

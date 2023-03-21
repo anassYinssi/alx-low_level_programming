ee0e9df6ac9628859ee3c136eaac8a232ef3b34d
anassYinssi <anass.yinssi@outlook.fr>
2023-03-21 06:15:58 -0700

    first quetion

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,13 +7,14 @@
 
 int main(void)
 {
-	int i;
-	char chaine = "_putchar";
-
-	for (i = 0; i < 8 ; i++)
-	{
-		_putchar(chaine[i]);
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 
 	return (0);

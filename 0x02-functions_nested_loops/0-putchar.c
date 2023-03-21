3a8f4b26f6d3154911c325a3a9d013d4e94331d0
anassYinssi <anass.yinssi@outlook.fr>
2023-03-21 06:02:23 -0700

    new commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,24 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include<unistd.h>
 /**
 * Main - Program entry
 *
 * Return: Always 0 (Success)
 */
 
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
 int main(void)
 {
-	printf("_putchar\n");
+	int i;
+	char chaine = "_putchar";
+	for(i = 0; i < 8 ; i++)
+	{
+		_putchar(chaine[i]);
+	}
+
 	return (0);
 }
+

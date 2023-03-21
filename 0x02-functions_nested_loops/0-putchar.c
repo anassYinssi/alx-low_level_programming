bc39b5ec4ffdeaefb5e7cd618795461726be24e0
anassYinssi <anass.yinssi@outlook.fr>
2023-03-21 06:13:19 -0700

    first quetion

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,23 +1,20 @@
-#include<unistd.h>
+#include "main.h";
 /**
 * Main - Program entry
 *
 * Return: Always 0 (Success)
 */
 
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
 int main(void)
 {
 	int i;
 	char chaine = "_putchar";
-	for(i = 0; i < 8 ; i++)
+
+	for (i = 0; i < 8 ; i++)
 	{
 		_putchar(chaine[i]);
 	}
+	_putchar('\n');
 
 	return (0);
 }

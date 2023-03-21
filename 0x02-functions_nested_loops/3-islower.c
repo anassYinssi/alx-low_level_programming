ed0f49a8ba3dc7013f685b2a41f9fbab2ba1e64f
anassYinssi <anass.yinssi@outlook.fr>
2023-03-21 16:10:17 -0700

    4 question

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* _islower - Program entry
+*
+* Return: Always 0 (Success)
+*/
+
+int _islower(int c)
+{
+	int i;
+	char c1;
+
+	for (i = 97; i <= 122; i++)
+	{
+		c1 = _putchar(i);
+		if (c1 == c)
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/3-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* main - Program entry
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	_islower('a');
+	return (0);
+}

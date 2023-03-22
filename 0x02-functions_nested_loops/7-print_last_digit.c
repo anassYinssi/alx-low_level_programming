79f166e9fb9f1d2b4e0012488f7097d7848134ea
anassYinssi <anass.yinssi@outlook.fr>
2023-03-21 17:33:27 -0700

    7 question

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,6 +7,13 @@
 
 int print_last_digit(int n)
 {
-	_putchar((n % 10) + 48);
-	return (0);
+	int last;
+
+	last = n % 10;
+	if (last < 0)
+	{
+		last = last * -1;
+	}
+	_putchar( last+ 48);
+	return (last);
 }

7752b0cd0b0ee85bd622377907c6e547fd872b1b
anassYinssi <anass.yinssi@outlook.fr>
2023-03-27 08:49:54 -0700

    Task2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include "main.h"
+/**
+* _strlen - Program entry
+* @s:xxxxxxxxx
+*
+* Return: Always 0 (Success)
+*/
+
+int _strlen(char *s)
+{
+	return (strlen(*s));
+}

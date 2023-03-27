d9ea4d6da078409d81f10934092beac5f9427d49
anassYinssi <anass.yinssi@outlook.fr>
2023-03-27 09:13:48 -0700

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include "main.h"
+/**
+* print_rev - Program entry
+*
+* Return: Always 0 (Success)
+*/
+
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = strlen(s); i > 0 ; i--)
+	{
+		printf("%s", *(s+i));
+	}
+	printf("\n");
+}

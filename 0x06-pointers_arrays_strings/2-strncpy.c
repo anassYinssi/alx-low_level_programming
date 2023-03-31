c141ba76a2743855699721fa05d6ab763eba6c63
anassYinssi <anass.yinssi@outlook.fr>
2023-03-31 09:51:42 -0700

    task2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,16 +11,17 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
-	int j;
 
-	while (dest[i] != '\0')
+	while (i < n && src[i] != '\0')
 	{
+		dest[i] = src[i];
 		i++;
 	}
 
-	for (j = 0; j < n; j++)
+	while (i < n)
 	{
-		dest[i] = src[j];
+		dest[i]='\0';
+		i++;
 	}
 
 	return (dest);

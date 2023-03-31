047b3d240f068669d87a5544878ab458d4e5a172
anassYinssi <anass.yinssi@outlook.fr>
2023-03-31 08:46:03 -0700

    task1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include "main.h"
+/**
+* _strncat - Program entry
+* @dest:xxxxx
+* @src: xxxx
+* @n:x xxx
+* Return: Always 0 (Success)
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (j < n)
+	{
+		if (src[j] == '\0')
+		{
+			break;
+		}
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+}

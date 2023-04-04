492be8668fd4640f461348c34d366035bae0bd93
anassYinssi <anass.yinssi@outlook.fr>
2023-04-03 19:46:57 -0700

    task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,12 +11,14 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	int i = 0;
+	int r = 0;
+	int i = n;
 
-	for (; n > 0 ; i++)
+	for (; r < i; r++)
 	{
-		dest[i] = src[i];
+		dest[r] = src[r];
 		n--;
 	}
 	return (dest);
 }
+

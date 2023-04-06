a95cae7be38817da0ecff839b5f436fdee9d259b
anassYinssi <anass.yinssi@outlook.fr>
2023-04-06 08:48:25 -0700

    task 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include "main.h"
+/**
+* _print_rev_recursion - Program entry
+* @s:xxxx
+*
+* Return: Always 0 (Success)
+*/
+
+void _print_rev_recursion(char *s)
+{
+
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+
+}

233a4cb733ebeac415f20adb2eb2b6dc4ee29412
anassYinssi <anass.yinssi@outlook.fr>
2023-04-18 08:19:40 -0700

    task0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+
+
+	/**
+	 * struct dog - a dog's basic info
+	 * @name: First member
+	 * @age: Second member
+	 * @owner: Third member
+	 *
+	 * Description: Longer description
+	 */
 typedef struct dog
 {
 	char *name;
@@ -8,3 +20,4 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+#endif

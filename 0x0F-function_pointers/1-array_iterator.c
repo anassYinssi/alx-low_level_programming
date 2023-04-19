6951d2272403f78a55ef27a9a930ce76f444173e
anassYinssi <anass.yinssi@outlook.fr>
2023-04-19 12:01:58 -0700

    2Task

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,11 +9,11 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	int i = 0;
+	int i;
 
 	if (array == NULL || action == NULL)
 		return;
-	for (; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		action(array[i]);
 	}

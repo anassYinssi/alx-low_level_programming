d7e67004b07dd5d24481cc25f5a47df9d5b9da69
anassYinssi <anass.yinssi@outlook.fr>
2023-04-21 09:23:14 -0700

    first task

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdarg.h>
+#include "main.h"
+/**
+* sum_them_all - Program entry
+* @n:xxxx
+*
+* Return: Always 0 (Success)
+*/
+
+int sum_them_all(const unsigned int n, ...)
+{
+	int sum = 0;
+	unsigned int i;
+	va_list args;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(args, int);
+	}
+	va_end(args);
+	return (sum);
+
+}
